0dc4539a5660ba6fc2aae996cb3a45589259eb4c
flygo <1020103647@qq.com>
2021-02-22 19:53:38 +0800

    feat: docker 运行mongo

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,6 @@ async function bootstrap() {
     .setTitle('nestjs 博客API')
     .setDescription('我的nestjs博客')
     .setVersion('1.0')
-    .addTag('blog')
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api-docs', app, document);
