10e78ca4f45572979a1e9ed10196e93a123ea808
pegion <1020103647@qq.com>
2021-02-27 22:21:46 +0800

    refine: 上传文件的命名

--- src/uploads/uploads.module.ts
+++ src/uploads/uploads.module.ts
@@ -3,7 +3,6 @@ import { MulterModule } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { UploadsController } from './uploads.controller';
 import { UploadsService } from './uploads.service';
-import * as nuid from 'nuid';
 
 @Module({
   imports: [
@@ -11,10 +10,8 @@ import * as nuid from 'nuid';
       storage: diskStorage({
         // 配置文件上传后的文件夹路径
         destination: `uploads`,
-        filename: (req, file, cb) => {
-          const filename = `${nuid.next()}.${file.originalname}`;
-          console.log(file);
-          return cb(null, filename);
+        filename: (_, file, cb) => {
+          return cb(null, file.originalname);
         },
       }),
     }),
